9c550b95e7be3684b974a1e2c0825f97b494b5a8
kamlno <ts02641998@gmail.com>
2021-12-09 20:19:16 +0800

    test01

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public static GameManager instance;
+    public static GameObject localPlayer;
+
+    string gameVersion = "1";
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogErrorFormat(gameObject,
+                    "Multiple instances of {0} is not allow", GetType().Name);
+            DestroyImmediate(gameObject);
+            return;
+        }
+        PhotonNetwork.AutomaticallySyncScene = true;
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+    }
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = gameVersion;
+    }
+    public override void OnConnected()
+    {
+        Debug.Log("PUN Connected");
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Connected to Master");
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Disconnected was called by PUN with reason {0}", cause);
+    }
+
+    public void JoinGameRoom()
+    {
+        var options = new RoomOptions
+        {
+            MaxPlayers = 6
+        };
+        PhotonNetwork.JoinOrCreateRoom("Kingdom", options, null);
+    }
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Created room!!");
+            PhotonNetwork.LoadLevel("GameScene");
+        }
+        else
+        {
+            Debug.Log("Joined room!!");
+        }
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (!PhotonNetwork.InRoom)
+        {
+            return;
+        }
+        localPlayer = PhotonNetwork.Instantiate("TankPlayer", new Vector3(0, 0, 0), Quaternion.identity, 0);
+        Debug.Log("Player Instance ID: " + localPlayer.GetInstanceID());
+    }
+}
