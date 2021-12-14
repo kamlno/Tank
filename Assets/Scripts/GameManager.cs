a32f076d5fe9584677f5de3ec9d0704256c04cbb
kamlno <ts02641998@gmail.com>
2021-12-14 20:16:32 +0800

    HUD02

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections.Generic;
+using Tanks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +9,7 @@ public class GameManager : MonoBehaviourPunCallbacks
 {
     public static GameManager instance;
     public static GameObject localPlayer;
+    private GameObject defaultSpawnPoint;
 
     string gameVersion = "1";
     void Awake()
@@ -21,8 +24,13 @@ public class GameManager : MonoBehaviourPunCallbacks
         PhotonNetwork.AutomaticallySyncScene = true;
         DontDestroyOnLoad(gameObject);
         instance = this;
-    }
-    void Start()
+
+        defaultSpawnPoint = new GameObject("Default SpawnPoint");
+        defaultSpawnPoint.transform.position = new Vector3(0, 0, 0);
+        defaultSpawnPoint.transform.SetParent(transform, false);
+
+        }
+        void Start()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
 
@@ -68,7 +76,39 @@ public class GameManager : MonoBehaviourPunCallbacks
         {
             return;
         }
-        localPlayer = PhotonNetwork.Instantiate("TankPlayer", new Vector3(0, 0, 0), Quaternion.identity, 0);
+
+        var spawnPoint = GetRandomSpawnPoint();
+        localPlayer = PhotonNetwork.Instantiate(
+            "TankPlayer", 
+            spawnPoint.position,
+            spawnPoint.rotation,
+            0);
         Debug.Log("Player Instance ID: " + localPlayer.GetInstanceID());
     }
+
+    #region Unity
+
+    private Transform GetRandomSpawnPoint()
+    {
+        var spawnPoints = GetAllObjectsOfTypeInScene<SpawnPoint>();
+        return spawnPoints.Count == 0
+        ? defaultSpawnPoint.transform
+        : spawnPoints[Random.Range(0, spawnPoints.Count)].transform;
+    }
+    
+    public static List<GameObject> GetAllObjectsOfTypeInScene<T>()
+    {
+        var objectsInScene = new List<GameObject>();
+        foreach (var go in (GameObject[])Resources.FindObjectsOfTypeAll(typeof(GameObject)))
+        {
+            if (go.hideFlags == HideFlags.NotEditable ||
+                go.hideFlags == HideFlags.HideAndDontSave)
+                continue;
+            if (go.GetComponent<T>() != null)
+                objectsInScene.Add(go);
+        }
+        return objectsInScene;
+    }
+
+    #endregion
 }
