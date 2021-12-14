02ecf6f1864f80ac351db94c8658390215ac819c
kamlno <ts02641998@gmail.com>
2021-12-14 21:32:11 +0800

    RPC02

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnPoint : MonoBehaviour
+namespace Tanks
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SpawnPoint : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Awake()
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
